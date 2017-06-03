b4eb59f477a47741b17b98bb7ab5fab20222bdc4
Parth Singh <apple@Apples-MacBook-Air.local>
2017-06-03 18:42:37 +0530

    Styling Added

--- backend.js
+++ backend.js
@@ -1,4 +1,5 @@
 function yolo(){
+
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(showPosition,showError);
 	}
@@ -17,6 +18,7 @@ function yolo(){
 					var temp=response.currently.temperature;
 					var weather=response.currently.summary;
 					var wind=response.currently.windSpeed;
+					getAddress(lat, lon);
 					document.getElementById('weather').innerHTML=weather;
 					document.getElementById('temp').innerHTML=temp;
 					document.getElementById('wind').innerHTML=wind;
@@ -36,4 +38,23 @@ function yolo(){
 	function showError(error){
 		document.getElementById('weather').innerHTML='ERROR CODE :'+error.code+'<br />'+error.message;
 	}
+	document.getElementById("check").innerHTML='Try Again';
+
+}
+function getAddress (latitude, longitude) {
+    
+        var request = new XMLHttpRequest();
+        request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=AIzaSyCXxPnGXsaV9Qjpte5LYE4H0SDFVMQOjSQ', true);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    var data = JSON.parse(request.responseText);
+                    var address = data.results[0].address_components[3].long_name;
+                    document.getElementById('add').innerHTML=address;
+                }
+                
+            }
+        };
+        request.send();
+    
 }
